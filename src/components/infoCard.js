0f3a77ba726575f41d5f1f3e0927f63ddeb1532f
David Roman <dmroman395@gmail.com>
2022-01-20 14:16:35 -0500

    Created newsCard, still needs styling

--- src/components/infoCard.js
+++ src/components/infoCard.js
@@ -1,23 +1,19 @@
+import React from 'react';
 import Box from '@mui/material/Box';
-import Tabs from '@mui/material/Tabs';
+import TabList from '@mui/lab/TabList';
 import Tab from '@mui/material/Tab';
+import TabPanel from '@mui/lab/TabPanel';
+import TabContext from '@mui/lab/TabContext';
 import { useState } from 'react';
-// import { selectDarkMode } from './redux/darkModeSlice';
-// import { selectTheme } from './redux/themeSlice'
-// import { useSelector } from 'react-redux';
-import { useTheme } from '@mui/material/styles';
 import { Paper, Typography } from '@mui/material';
-
-
+import NewsCard from './newsCard';
 
 function InfoCard() {
-    const [tabValue, setTabValue] = useState(0);
+    const [tabValue, setTabValue] = useState('1');
 
     const handleChange = (e, newVal) => {
       setTabValue(newVal);
     };
-
-    const theme = useTheme()
 
     return(
         <Paper
@@ -27,30 +23,37 @@ function InfoCard() {
                 width: '500px'
             }}
         >
-            <Box
+            <Typography 
+                variant='h4' 
+                align='center'
                 sx={{
-                    borderBottom: 1, 
-                    borderColor: 'divider',
-                    borderTopLeftRadius: 3,
-                    borderTopRightRadius: 3
+                    margin: '15px 0'
                 }}
             >
-                <Typography 
-                    variant='h4' 
-                    align='center'
+                Country
+            </Typography>
+            <TabContext value={tabValue}>
+                <Box
                     sx={{
-                        margin: '15px 0'
+                        borderBottom: 1, 
+                        borderColor: 'divider',
+                        borderTopLeftRadius: 3,
+                        borderTopRightRadius: 3
                     }}
                 >
-                    Country
-                </Typography>
-                <Tabs value={tabValue} variant='fullWidth' onChange={handleChange}>
-                    <Tab label='News'></Tab>
-                    <Tab label='Stats'></Tab>
-                </Tabs>
-            </Box>
-        </Paper>
-        
+                    <TabList variant='fullWidth' onChange={handleChange}>
+                        <Tab label='News' value='1'/>
+                        <Tab label='Stats' value='2'/>
+                    </TabList>
+                </Box>
+                <TabPanel value='1'>
+                    <NewsCard/>
+                </TabPanel>
+                <TabPanel value='2'>
+                    Stats
+                </TabPanel>
+            </TabContext>
+        </Paper>  
     )
 }
 
